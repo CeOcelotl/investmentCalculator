69d03eafeaeb265640f3d3441ebe0841edd53786
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2023-08-03 18:45:13 +0800

    Outputting Results Conditionally

--- src/App.js
+++ src/App.js
@@ -32,12 +32,9 @@ function App() {
   return (
     <div>
       <Header />
-
       <UserInput onCalculate={calculateHandler} />
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-
-      <ResultsTable />
+     {!userInput && <p>No investment calculated yet.</p>}
+     {userInput &&<ResultsTable data={yearlyData} initialInvestment={userInput['currentSavings']}/>}
     </div>
   );
 }
--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,25 +1,34 @@
-const ResultsTable = ()=>{
-    return (
-        <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
+const formatter = new Intl.NumberFormat('en-US',{
+  style: 'currency', //設定數字的風格， currency 為貨幣
+  currency: 'USD', //貨幣代碼
+  minimumFractionDigits:2,//設定小數部分的最小和最大位數
+  maximumFractionDigits:2
+})
+
+const ResultsTable = (props) => {
+  return (
+    <table className="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((yearData) => (
+          <tr key={yearData.year}>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+            <td>{formatter.format(yearData.yearlyInterest)}</td>
+            <td>{formatter.format(yearData.savingsEndOfYear - props.initialInvestment - yearData.yearlyContribution * yearData.year)}</td>
+            <td>{formatter.format(props.initialInvestment + yearData.yearlyContribution * yearData.year)}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
-    )
+        ))}
+      </tbody>
+    </table>
+  );
 };
 export default ResultsTable;
